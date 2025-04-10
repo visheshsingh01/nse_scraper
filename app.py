cb3320713158d05b3ff7471b594e2312247a06ed
visheshsingh01 <vishesh@brancosoft.com>
2025-04-10 23:05:09 +0530

    minor cgange uin the driver of the chrome

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ def setup_driver(headless=True):
     options.add_argument("--disable-blink-features=AutomationControlled")  # ✅ Prevent bot detection
     options.add_argument("--start-maximized")
 
+    options.add_argument(f"--user-data-dir=/tmp/chrome-user-data-{time.time()}")
+
     # ✅ Set a Real User-Agent to Bypass Detection
     options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.7049.84 Safari/537.36"
